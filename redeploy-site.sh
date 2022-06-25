e0fe8d121e769bfb87c04e2e1ab6851f0dba2170
SreevidyaAlivelu0106 <sxa200150@utdallas.edu>
2022-06-25 14:25:22 -0500

    changes made to the reseploy-site.sh

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-
 cd portfolio
 git fetch
 git reset origin/main --hard
-python -m venv python3-virtualenv
-source python-virtualenv/bin/activate
+python -m venv python-virtualenv
+source python3-virtualenv/bin/activate
 pip install -r requirements.txt
-Restart myportfolio service
+systemctl daemon-reload
+systemctl restart myportfolio
+systemctl status myportfolio
