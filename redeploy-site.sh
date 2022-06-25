c3b97362948d0341e9afd766402e2f6e95617526
SreevidyaAlivelu0106 <sxa200150@utdallas.edu>
2022-06-25 14:11:18 -0500

    added service file

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
-pkill -f tmux
+
 cd portfolio
 git fetch
 git reset origin/main --hard
 python -m venv python3-virtualenv
-source python3-virtualenv/bin/activate
+source python-virtualenv/bin/activate
 pip install -r requirements.txt
-
-dnf install tmux
-tmux new -s "remote" -d
-tmux send-keys -t "remote" "./ss.sh" C-m
-tmux attach -t "remote" -d
+Restart myportfolio service
