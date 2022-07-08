1a6315dc2427d20af886c13c38c7dc4806bed447
SreevidyaAlivelu0106 <sxa200150@utdallas.edu>
2022-07-08 18:02:32 -0500

    changes made in re-deployment script

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -2,9 +2,5 @@
 cd portfolio
 git fetch
 git reset origin/main --hard
-python -m venv python-virtualenv
-source python3-virtualenv/bin/activate
-pip install -r requirements.txt
-systemctl daemon-reload
-systemctl restart myportfolio
-systemctl status myportfolio
+docker compose -f docker-compose.prod.yml down
+docker compose -f docker-compose.prod.yml up -d --build
